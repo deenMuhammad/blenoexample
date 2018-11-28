b33d515a1808ec481d03ab90e4a9263d72ea8081
deenMuhammad <zorro199696@gmail.com>
2018-11-28 14:14:49 +0900

    Pyhton Extension started

---
+++ python/test.js
@@ -0,0 +1,14 @@
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',["./test.py"]);
+
+pythonProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  pythonProcess.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  
+  pythonProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
